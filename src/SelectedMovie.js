4ec1623c613737f7fcff11528b60cae35993c6af
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2023-12-13 14:46:18 +0000

    title changes

--- src/SelectedMovie.js
+++ src/SelectedMovie.js
@@ -52,6 +52,15 @@ export function SelectedMovie({
     }
     fetchMovieDetails();
   }, [selectedId]);
+
+  useEffect(
+    function () {
+      if(!title) return;
+      document.title = `Movie |${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
